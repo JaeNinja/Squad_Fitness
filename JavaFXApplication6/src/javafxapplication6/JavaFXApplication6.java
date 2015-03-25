257b1e43d26e32a6b422b0849385fba8c0eace01
JaeNinja <jgardner21@student.gsu.edu>
2015-03-25 10:35:19 -0400

    Update

--- JavaFXApplication6/src/javafxapplication6/JavaFXApplication6.java
+++ JavaFXApplication6/src/javafxapplication6/JavaFXApplication6.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javafxapplication6;
 
 import javafx.application.Application;
@@ -11,25 +7,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Jae
- */
+
+
 public class JavaFXApplication6 extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         
+        stage.setTitle("Welcome to the SQUAD");
         stage.setScene(scene);
         stage.show();
     }
-
-    /**
-     * @param args the command line arguments
-     */
+    
+  
+//
+//    /**
+//     * @param args the command line arguments
+//     */
     public static void main(String[] args) {
         launch(args);
     }

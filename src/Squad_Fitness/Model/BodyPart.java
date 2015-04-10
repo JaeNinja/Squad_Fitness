db71ca6290758b5456821a16b26f77953f5d4317
Nathaniel Zemui <Nathanielzemui_5@yahoo.com>
2015-04-09 21:16:51 -0400

    Added constructors.

--- src/Squad_Fitness/Model/BodyPart.java
+++ src/Squad_Fitness/Model/BodyPart.java
@@ -6,7 +6,31 @@ package Squad_Fitness.Model;
 public class BodyPart {
 
     int bodyPartID;
-    String name;
+    String Name;
+    String Shoulders;
+    String Back;
+    String Biceps;
+    String Triceps;
+    String Forearms;
+    String Chest;
+    String Abs;
+    String Quads;
+    String Hamstrings;
+    String Calves;
 
-    public BodyPart() {}
+    public BodyPart(int bodypartid, String name, String shoulders, String back, String biceps, String triceps, String forearms,
+                    String chest, String abs, String quads, String hamstrings, String calves) {
+        bodyPartID = bodypartid;
+        Name = name;
+        Shoulders = shoulders;
+        Back = back;
+        Biceps = biceps;
+        Triceps = triceps;
+        Forearms = forearms;
+        Chest = chest;
+        Abs = abs;
+        Quads = quads;
+        Hamstrings = hamstrings;
+        Calves = calves;
+    }
 }
--- src/Squad_Fitness/Model/healthyFood.java
+++ src/Squad_Fitness/Model/healthyFood.java
@@ -7,16 +7,30 @@ public class healthyFood {
 
     String foodName;
     String servingSize;
-    int calories;
+    int calorieS;
     int caloriesFromFat;
     int totalFat;
     int saturatedFat;
-    int cholesterol;
-    int sodium;
-    int carbohydrate;
-    int fiber;
-    int sugar;
-    int protein;
+    int cholesteroL;
+    int sodiuM;
+    int carbohydratE;
+    int fibeR;
+    int sugaR;
+    int proteiN;
 
-    public healthyFood() {}
+    public healthyFood(String foodname, String servingsize, int calories, int caloriesfromfat, int totalfat,
+        int saturatedfat, int cholesterol, int sodium, int carbohydrate, int fiber, int sugar, int protein) {
+            foodName = foodname;
+            servingSize = servingsize;
+            calorieS = calories;
+            caloriesFromFat = caloriesfromfat;
+            totalFat = totalfat;
+            saturatedFat = saturatedfat;
+            cholesteroL = cholesterol;
+            sodiuM = sodium;
+            carbohydratE = carbohydrate;
+            fibeR = fiber;
+            sugaR = sugar;
+            proteiN = protein;
+    }
 }

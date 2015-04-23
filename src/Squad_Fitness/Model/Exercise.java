e74826e34f35bf19e6e8919c9c7092f62a1430e9
Kyle Sudu <kyle@akadasoftware.com>
2015-04-22 21:03:48 -0400

    Exercise is long af

--- src/Squad_Fitness/Model/Exercise.java
+++ src/Squad_Fitness/Model/Exercise.java
@@ -31,12 +31,83 @@ public class Exercise {
         equipmentNeeded = equipmentneeded;
     }
 
-    public int getExerciseID() {
+    public int getExerciseID()
+    {
         return exerciseID;
     }
 
-    public void setExerciseID(int newExerciseID) {
+    public void setExerciseID(int newExerciseID)
+    {
         exerciseID = newExerciseID;
     }
 
+    public String getExerciseName()
+    {
+        return exerciseName;
+    }
+
+    public void setExerciseName(String ExerciseName)
+    {
+        exerciseName = ExerciseName;
+    }
+
+    public String getExerciseDesription()
+    {
+        return exerciseName;
+    }
+
+    public void setExerciseDescription(String ExerciseDescription)
+    {
+        exerciseDescription = ExerciseDescription;
+    }
+
+    public String getType()
+    {
+        return typE;
+    }
+
+    public void setType(String Type)
+    {
+        typE = Type;
+    }
+
+    public String getTips()
+    {
+        return tipS;
+    }
+
+    public void setTips(String Tips)
+    {
+        tipS = Tips;
+    }
+
+    public int getBodyPart()
+    {
+        return bodyPart;
+    }
+
+    public void setBodyPart(int BodyPart)
+    {
+        bodyPart = BodyPart;
+    }
+
+    public Boolean getEquipmentNeeded()
+    {
+        return equipmentNeeded;
+    }
+
+    public void setEquipmentNeeded(boolean EquipmentNeeded)
+    {
+        equipmentNeeded = EquipmentNeeded;
+    }
+
+    public int getTimeRequired()
+    {
+        return timeRequired;
+    }
+
+    public void setTimeRequired(int TimeRequired)
+    {
+        timeRequired = TimeRequired;
+    }
 }
